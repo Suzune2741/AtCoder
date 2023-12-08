1e0bf017a2e36a91b23cadc28f6b609fc489cf65
Suzune2741 <kannra1174@gmail.com>
2023-12-08 11:11:09 +0900

    ファイル整理

--- abc304~317/abc306/a.rb
+++
@@ -1,7 +0,0 @@
-n = gets.to_i
-s =gets.chars
-n.times do |i|
-    print s[i]
-    print s[i]
-end
-puts 
--- abc304~317/abc312/b.rb
+++
@@ -1,9 +0,0 @@
-n,m = gets.split().map(&:to_i)
-map=Array.new(n){Array.new(m)}
-
-n.times do |i|
-    map[i] = gets.split
-end
-n.times do |i|
-    
-end
