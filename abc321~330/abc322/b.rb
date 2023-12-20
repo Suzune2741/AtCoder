63851c6669257f6a210340d18b7925fdb824ad4d
Suzune2741 <kannra1174@gmail.com>
2023-12-20 21:49:25 +0900

    abc322:b WAを解消

--- abc321~330/abc322/b.rb
+++ abc321~330/abc322/b.rb
@@ -2,18 +2,14 @@ n,m = gets.split.map(&:to_i)
 s = gets.chomp
 t = gets.chomp
 
-if t.start_with?(s)
-    if t.end_with?(s)
-        ans = 0
-    else
-        ans = 1
-    end
-  else
-    if t.end_with?(s)
-        ans = 2
-    else
-        ans = 3
-    end
-  end
+if t.start_with?(s) && t.end_with?(s)
+    ans = 0
+elsif t.start_with?(s)
+    ans = 1
+elsif t.end_with?(s)
+    ans = 2
+else
+    ans = 3
+end
 
 puts ans
