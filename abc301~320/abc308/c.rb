3df6236b989bcb5df2351c8392994bb8e6b06594
Suzune2741 <kannra1174@gmail.com>
2023-12-29 18:13:12 +0900

    ABC308 C

--- abc301~320/abc308/c.rb
+++ abc301~320/abc308/c.rb
@@ -1,29 +1,13 @@
 n = gets.to_i
-ans = []
-ac = []
-bef = -1
+a = []
+b = []
 n.times do |i|
-    a,b = gets.split.map(&:to_i)
-    res = a.to_f/(a+b)
-    if
-        ac[i] = res 
-    end
-    bef = ac[i]
+    a[i],b[i] = gets.split.map(&:to_f)
+end
+num = {}
+n.times do |i|
+    num[i+1] = Rational(a[i],(a[i] + b[i]))
 end
 i = 0
-flag = 0
-ans = ac.each_with_index.sort_by {|v| [v, i += 1] } .reverse
-0.upto(n-1) do |j|
-    if ans[j][0] == ans[j-1][0] 
-        print "#{ans[j][1]+1} "
-        flag = 1
-    elsif flag == 0
-        print "#{ans[j][1]+1} "
-        flag = 0
-    else
-        print "#{ans[j][1]+1} "
-        flag = 0
-    end
-    
-end
-
+ans = num.sort_by{|k,v| [-v,i+=1]}.map{|k,v| k}
+puts ans.join(" ")
