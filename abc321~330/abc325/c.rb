bab68fc2614fa460f956e697cb32da6ebbfa03de
Suzune2741 <kannra1174@gmail.com>
2024-01-17 16:34:28 +0900

    Refactor code to count connected components in a grid

--- abc321~330/abc325/c.rb
+++ abc321~330/abc325/c.rb
@@ -1,9 +1,30 @@
 h,w = gets.split.map(&:to_i)
-s = []
+$s = []
 h.times do |i|
-    s[i] = gets.chomp.split("")
+    $s[i] = gets.chomp.split("")
 end
-s = s.join!
-p s
-fnum = s.index("#")
-
+def near(i,j,h,w)
+    (-1..1).each do |k|
+        (-1..1).each do |l|
+            next if k==0 &&l == 0 
+            ni,nj = i+k,j+l
+            if  ni>= 0 && nj >= 0 && nj <= w && ni < h
+                if $s[ni][nj] == "#"
+                    $s[ni][nj] = 1
+                    near(ni,nj,h,w)
+                end
+            end
+        end
+    end
+end
+count = 0
+h.times do |i|
+    w.times do |j|
+        if $s[i][j] == "#"
+            $s[i][j] = 1
+            near(i,j,h,w)
+            count+=1
+        end
+    end
+end
+puts count
