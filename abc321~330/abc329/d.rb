41a2280931a1c6b2789155df1dca1ceba933514a
Suzune2741 <kannra1174@gmail.com>
2024-02-24 20:03:25 +0900

    abc329 d AC

--- abc321~330/abc329/d.rb
+++ abc321~330/abc329/d.rb
@@ -1,19 +1,17 @@
 n,m = gets.split.map(&:to_i)
 ai = gets.split.map(&:to_i)
-
-pe = Hash.new(0)
-
-
-m.times do |i|
-    sym = ai[i].to_s.to_sym
-    pe[sym] = pe[sym] + 1
-    max = pe.max{ |x, y| x[1] <=> y[1] }
-    p max
-    min =  pe.max{ |x, y| x[1] <=> y[1] }
-    p min
-    if min.size != 1
-        puts min[0]
-    else
-        puts min
+hash = {}
+ans = []
+ai.each do |a|
+    hash[a]||= 0
+    hash[a] += 1
+    max = hash.values.max
+    min = 1000000000000000
+    hash.each do |k,v|
+        if v == max && k<=min
+            min = k
+        end
     end
+    ans << min
 end
+puts ans.join("\n")
