0a2aa5c2744f781200c8b4ae087fbd1383e4fc91
Suzune2741 <kannra1174@gmail.com>
2024-02-24 20:38:49 +0900

    abc309 C

--- abc301~320/abc309/c.rb
+++ abc301~320/abc309/c.rb
@@ -1,29 +1,21 @@
 n,k = gets.split.map(&:to_i)
-med=Array.new()
-sums = Array.new()
+hash = Hash.new(0)
 sum = 0
 n.times do |i|
-    med[i] = gets.split.map(&:to_i)
-    sum += med[i][1]
-    if(i<1)
-        sums[i] =sums[i-1] + med[i][1] + med[i-1][1]
-    else
-        sums[0] =med[i][1]
-    end
+    a,b = gets.split.map(&:to_i)
+    hash[a] += b
+    sum += b
 end
-puts sums.min_by{|x| (x-k).abs}
-smed = med.sort
-now = 0
-bef = 1
-while 1 do
-    if smed[now][0] < x
-        sum -= smed[now][1]
-        now += 1
-    end
+if sum <= k
+    puts 1
+    exit
+end
+bef = 0
+hash.sort.each do |a,b|
+    sum -= b
+    bef = a
     if sum <= k
-        puts x
+        puts bef + 1
         exit
     end
-    x+=1
-        
 end
