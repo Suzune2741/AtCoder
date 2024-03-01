f6a69b2f0e0750ffb46dc6e4a7cf5deaed6d63b9
Suzune2741 <kannra1174@gmail.com>
2024-03-02 02:17:42 +0900

    典型#10

--- Typical/10.rb
+++ Typical/10.rb
@@ -2,18 +2,27 @@
 n = gets.to_i
 po1 = Array.new(n,0)
 po2 = Array.new(n,0)
-n.times do |i|
-    clas,po=gets.split.map(&:to_i)
-    if  clas==1
-        po1[i]=po
+clas = Array.new(n,0)
+po = Array.new(n,0)
+(1..n).each do |i|
+    clas[i],po[i]=gets.split.map(&:to_i)
+end
+
+(1..n).each do |i|
+    if clas[i] == 1
+        po1[i]= po1[i-1]+po[i]
+        po2[i] = po2[i-1]
     else
-        po2[i]=po
+        po2[i] = po2[i-1]+po[i]
+        po1[i] = po1[i-1]
     end
 end
 Q=gets.to_i
 res=[]
-Q.times do 
+Q.times do
     a,b=gets.split.map(&:to_i)
-    res.push "#{po1[a-1..b-1].sum} #{po2[a-1..b-1].sum}"
+    ans1 = po1[b]-po1[a-1]
+    ans2 = po2[b]-po2[a-1]
+    res.push "#{ans1} #{ans2}"
 end
 puts res
