66e93cc4653919f62f238c8707656558e41fb9af
Suzune2741 <kannra1174@gmail.com>
2024-05-06 15:23:16 +0900

    abc305 c

--- abc301~320/abc305/c.rb
+++ abc301~320/abc305/c.rb
@@ -1,10 +1,23 @@
 h, w = gets.split.map(&:to_i)
 map =Array.new()
-res=/(\w*)\#+.{1}#*/
 h.times do |i|
-    map[i]=gets.chomp
-    puts /\#{1}.{1}\#{1}/==map[i]
-
+    map[i]=gets.chomp.split("")
 end
-#　#.#の時とかの座標出す感じ？
-#　端っこの時上下左右に3つあればそれっぽい
+div = [[1,0],[0,1],[-1,0],[0,-1]]
+h.times do |i|
+    w.times do |j|
+        if map[i][j] == "."
+            count = 0
+            div.each do |x,y|
+                next if i+y < 0 || h <= i+y || j+x < 0 || w <= j+x
+                if map[i+y][j+x] == "#"
+                    count += 1
+                end
+            end
+            if count >=2
+                puts "#{i+1} #{j+1}"
+                exit
+            end
+        end
+    end
+end
